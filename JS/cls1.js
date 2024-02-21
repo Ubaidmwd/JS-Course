55c91f9a6054129362d923725be1da9ec3a4ae6c
Ubaidmwd <ubaidmwd@gmail.com>
2024-02-21 11:16:29 +0500

    variable

--- JS/cls1.js
+++ JS/cls1.js
@@ -1,3 +1,16 @@
-var a = "classs one";
-var a = 123213;
-console.log(a);
+// 1st method for declaration of variable
+// var a = "classs one";
+// var a = 123213;
+// console.log(a);
+
+// 2nd method for declaration of variable
+
+// let a = "classs one";
+// a = "abc";
+// console.log(a);
+
+// 3rd method for declaration of variable
+
+// const a = "classs one";
+// a = "abc";
+// console.log(a);
